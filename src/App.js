f7535bf40ce05581883bed40fedefe01fce1a295
samuelguedesalves <samuelguedes7070@gmail.com>
2020-01-03 17:32:12 -0400

    login page update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React from 'react';
 //import logo from './logo.svg';
+import logo from './assets/logo_check_task.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="app-body" >
 
-        <div className='container pt-4' >
-
-          <div className="card text-dark">
-              <div className="card-body">
-                  <h2 className="card-title">Login <i className="fas fa-flag"></i> </h2>
-                  
-                  <div className="" >
-                      <div className="form-group">
-                          <label >Email address</label>
-                          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                      </div>
-                      <div className="form-group">
-                          <label >Password</label>
-                          <input type="password" className="form-control" id="exampleInputPassword1"/>
-                      </div>
-                      <button className="btn btn-primary container">Submit</button>
-                  </div>
-              </div>
-          </div>
-          <h5 className='mt-2' >Created by: Samuel Guedes</h5>
+        <div className="App-header">
+          <img className="logo-app" src={logo} alt="Check Task" />
         </div>
-      
         
+        <div className="center-div">
+          
+          <div className='login-form' >
+            <h3>Login</h3>
+            <div className="inputs-form" >
+              <input type="email" placeholder="E-mail" />
+              <input type="password" placeholder="Password" />
+            </div>
+            <button className="button-login" >Login</button>
+            <p> You not have a acount? <label className="link-create" >lest does create!</label> </p>
+          </div>
 
-      </header>
+        </div>
+
+
+      </div>
     </div>
   );
 }
