ea025b6d6a41bef5b0c79a72e3d275aedca0ebd3
samuelguedesalves <samuelguedes7070@gmail.com>
2020-01-03 22:56:14 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React from 'react';
+import React , { Component } from 'react';
 //import logo from './logo.svg';
 import logo from './assets/logo_check_task.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="app-body" >
 
-        <div className="App-header">
-          <img className="logo-app" src={logo} alt="Check Task" />
-        </div>
-        
-        <div className="center-div">
-          
-          <div className='login-form' >
-            <h3>Login</h3>
-            <div className="inputs-form" >
-              <input type="email" placeholder="E-mail" />
-              <input type="password" placeholder="Password" />
-            </div>
-            <button className="button-login" >Login</button>
-            <p> You not have a acount? <label className="link-create" >lest does create!</label> </p>
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email_handler: "",
+      password_handler: ""
+    }
+
+    this.handlerInput = this.handlerInput.bind(this);
+
+  }
+
+  handlerInput(e){
+    if(e.target.name === "email"){
+      this.setState({ email_handler: e.target.value });
+    }else{
+      if(e.target.name === "password"){
+        this.setState({ password_handler: e.target.value });
+      }
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container" >
+  
+          <div className="app-header">
+            <img className="logo-app" width="200px" src={logo} alt="Check Task" />
           </div>
-
+          
+          <div className="form-container" >
+          
+            <div className="form_login container" >
+                <h2 className="mb-5" >Login</h2>
+                <div className="form-group">
+                    <h5 >Email <i className="fas fa-envelope ml-2"></i> </h5>
+                    <input type="email" name="email" className="form-control" onChange={this.handlerInput} />
+                </div>
+                <div className="form-group">
+                    <h5 >Password <i className="fas fa-key ml-2"></i> </h5>
+                    <input type="password" name="password" className="form-control" onChange={this.handlerInput} />
+                </div>
+                <button className="btn btn-primary container">Login <i className="fas fa-door-open ml-2"></i> </button>
+                <h6  className="mt-3">You dont hava a acount? Let's make.</h6>
+            </div>
+  
+          </div>
+  
+  
         </div>
-
-
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
