39de2a46d8cb4f1cada94077e8c7fba44accea0f
samuelguedesalves <samuelguedes7070@gmail.com>
2020-01-06 22:50:05 -0400

    add new

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 import Logo from "../../assets/logo_check_task.svg";
 import "./Body.css";
@@ -11,14 +12,45 @@ class Body extends Component {
         super(props);
         
         this.state = {
-            user_token: ""
+            user_token: "",
+            textEdit: "olá mundo",
+            tasks: [],
         }
+
+        this.editNote = this.editNote.bind(this);
+    }
+
+    
+    componentDidMount(){
+        axios.post("https://api-carrot.herokuapp.com/my_task", {
+            user_token: localStorage.getItem("check_task_token")
+        }).then((api_response) => {
+            this.setState({ tasks: api_response.data });
+            console.log(api_response.data);
+        }).catch((err)=>{
+            console.error(err);
+        });
+    }
+
+    editNote(e){
+        console.log(e);
+
     }
 
     render() {
         if(localStorage.getItem("check_task_token") != null ){
             return (
                 <div className="app_container">
+                    
+                    {/*modal de text*/}
+                    <div className="modal_note">
+                        <div className="modal_content">
+                            <span className="close_modal"><i className="fas fa-times-circle"></i></span>
+                            <h3>Modal Title</h3>
+                            <textarea className="text_area_modal" >{this.state.textEdit}</textarea>
+                        </div>
+                    </div>
+
                     <div className="middler_container container" >
                         <div className="header_container container">
                             <img className="" width="200px" src={Logo} alt="Check Task" />
@@ -29,8 +61,12 @@ class Body extends Component {
                             
                             <div className="note_list">
                                 <button type="button" className="btn btn-success container mb-3">New Note <i className="fas fa-plus-circle ml-2"></i></button>
-
-                                <Note text_note="olá mundo" />
+                                <div>
+                                    {
+                                        this.state.tasks.map((item) => { return (<Note edit_note={this.editNote} key={item.task_id} text_note={item.task_note} />)})
+                                    }
+                                </div>
+                                
     
                             </div>
     
--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import "./Note.css";
 
 class Note extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            text: this.props.text_note
+        }
+    }
+
     render() {
         return (
-            <div className="card_container">
+            <div onClick={this.props.edit_note} className="card_container">
                 <p className="card_text">
-                    {this.props.text_note}
+                    {this.state.text}
                 </p>
             </div>
         );
